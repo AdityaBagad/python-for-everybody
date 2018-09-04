329619e21760e3ece484b95416185744e8dd079d
AdityaBagad <adityabagad@gmail.com>
2018-09-04 21:46:59 +0530

    Chapter 4

--- 4-Chapter/Exercise_1_2_3_4_5.py
+++ 4-Chapter/Exercise_1_2_3_4_5.py
@@ -27,15 +27,15 @@ Move the last line of this program to the top, so the function
 call appears before the definitions. Run the program and see what error
 message you get.
 """
-# repeat_lyrics()
+repeat_lyrics()
 
-# def print_lyrics():
-#     print("I'm a lumberjack, and I'm okay.")
-#     print('I sleep all night and I work all day.')
+def print_lyrics():
+    print("I'm a lumberjack, and I'm okay.")
+    print('I sleep all night and I work all day.')
 
-# def repeat_lyrics():
-#     print_lyrics()
-#     print_lyrics()
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
 
 """
 Traceback (most recent call last):
